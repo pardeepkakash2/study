cbeaa8899fc44b1eb4c31520a19e41266595f0b4
Dharmender Suresh Chander <Dharmender.SureshChander@in.bosch.com>
2017-07-26 11:48:43 +0530

    device spacific callback and container_of example update
    Signed-off-by: Dharmender Suresh Chander <Dharmender.SureshChander@in.bosch.com>

--- c/preprocessors/container_of.c
+++ c/preprocessors/container_of.c
@@ -1,3 +1,4 @@
+/* http://radek.io/2012/11/10/magical-container_of-macro/ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,6 +18,32 @@ struct test2 {
 	int c;
 };
 
+void zero_pointer_dereferance(void)
+{
+	/*This will print 53*/
+	printf("zero_pointer_dereferance c: %p\n", &( (struct test2 *) 0 )->c);
+	/*the below statment create segmantation falt why ?*/
+	//printf("%d\n", ( (struct test2 *) 0 )->c);
+	return;
+}
+
+void offset_test(void)
+{
+	printf("ofset of c: %ld\n", offsetof(struct test2, c));
+	printf("ofset of a: %ld\n", offsetof(struct test1, a));
+	
+	return;
+}
+
+void typeof_test(void)
+{
+	int x = 5;
+	/* Note : typeof(((struct test2 *)0)->c) c; => char c;*/
+	typeof(x) y = 6;
+	printf("x: %d, y: %d\n", x, y);
+	return;
+}
+
 int main()
 {
 	/* existing structure */
@@ -33,8 +60,12 @@ int main()
 	/* pointer to existing entry */    
 	struct test1 *obj1 = &obj->z;
 	struct test2 *obj2 = container_of(obj1, struct test2, z);
+	
 	printf("container_of:%p : obj2->b = %d\n", obj2, obj2->b);
-	printf("ofset of c: %ld\n",offsetof(struct test2, c));
-	printf("ofset of a: %ld\n",offsetof(struct test1, a));
+	
+	zero_pointer_dereferance();
+	typeof_test();
+	offset_test();
+
 	return EXIT_SUCCESS;
 }
